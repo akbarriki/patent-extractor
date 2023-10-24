f507857c7083aa25ae1f97b38e2d8146c5febb07
akbarriki <rikiakbar.pajak@gmail.com>
2023-10-24 10:34:13 +0700

    adding delay during scraping

--- patents.py
+++ patents.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import requests, warnings, sys
+import requests, warnings, sys, time
 from bs4 import BeautifulSoup as bs
 warnings.filterwarnings("ignore")
 
@@ -18,36 +18,60 @@ class Patents():
 
   def get_patent_text(self, patent_id:str):
 
-    lang_ = "en" if patent_id[:2] in ("WO","US") else "zh"
+    lang_ = "zh" if patent_id[:2] == "CH" else "en"
     url = f"https://patents.google.com/patent/{patent_id}/{lang_}"
 
     resp = requests.get(url)    
     if resp.ok:
       try:
         soup = bs(resp.content, 'html.parser')
-        title = soup.title.get_text().split("-")[1].strip()
-        abstract = soup.find("div",{"class":"abstract"}).get_text().strip()
-        description = soup.find("section",{"itemprop":"description"}).get_text().strip()
-        claims = soup.find("section",{"itemprop":"claims"}).get_text().strip()
+        content_ = ""
+        
+        try:
+          content_ += soup.title.get_text().split("-")[1].strip() + "\n\n"
+        except:
+          pass
 
-        content_ = f"{title}\n\n{abstract}\n\n{description}\n\n{claims}"
+        try:
+          content_ += soup.find("div",{"class":"abstract"}).get_text().strip() + "\n\n"
+        except:
+          pass
+        
+        try:
+          content_ += soup.find("section",{"itemprop":"description"}).get_text().strip() + "\n\n"
+        except:
+          pass
 
-        with open(f"{self.patent_tgt_dir}{patent_id}_{title}.txt","w", encoding="utf-8") as fw:
+        try:
+          content_ += soup.find("section",{"itemprop":"claims"}).get_text().strip()
+        except:
+          pass
+
+        with open(f"{self.patent_tgt_dir}/{patent_id}.txt","w", encoding="utf-8") as fw:
             fw.write(content_)
         return 1
       except:
+        print(self.patent_tgt_dir)
+        print(patent_id)
+        print(resp.status_code)
         return 0
     
   def get_patent_bulk(self):
-    for patent_id in self.patent_ids:
+    delay = 300
+    for i, patent_id in enumerate(self.patent_ids):
+      if i > 0  and i % 800 == 0:
+        time.sleep(delay)
+        print(f"*** Delay {delay} s***")
       print(f"loading {patent_id}...", end="")
       status = "Ok" if self.get_patent_text(patent_id) else "Not Ok"
+      if status == "Not Ok":
+        break
       print(f"complete (status:{status})")
 
 
 if __name__ == "__main__":
 #   source_file = "patent_num.txt"
-#   target_dir = "target/"
+#   target_dir = "target"
   patents = Patents()
   patents.get_patent_bulk()
 #   print(patents.get_patent_text("WO2005062560A1"))
